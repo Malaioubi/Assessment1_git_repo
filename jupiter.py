67dd97642da178496e32167c1d61bf4254f1c3e2
Malaioubi <157553291+Malaioubi@users.noreply.github.com>
2024-01-24 02:15:34 +0000

    created the Moons class jupiter.py

--- jupiter.py
+++ jupiter.py
@@ -7,3 +7,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
+
+class Moons:
+  
