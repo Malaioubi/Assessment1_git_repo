bd14a0c715a9c969898474c4a5ed81f9db44c419
Malaioubi <157553291+Malaioubi@users.noreply.github.com>
2024-01-24 02:18:35 +0000

    constructing and deconstructing functions jupiter.py

--- jupiter.py
+++ jupiter.py
@@ -9,4 +9,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
 
 class Moons:
-  
+    def __init__(self, database_path):
+        self.conn = sqlite3.connect(database_path)
+        self.load_data()  # Load moon data from the database
+        self.define_constants()  # Define constants including gravitational constant and moon mass
+      
+    def __del__(self):
+        self.conn.close()
